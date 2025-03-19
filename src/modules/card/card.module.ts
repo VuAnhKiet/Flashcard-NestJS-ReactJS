941db26e9c514559ec8918637f99dc6d331c25a1
Kiet <honeywork175@gmail.com>
2025-03-19 17:28:40 +0700

    Card's module

--- src/modules/card/card.module.ts
+++ src/modules/card/card.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CardService } from './card.service';
 import { CardController } from './card.controller';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Card } from 'src/entities/card.entity';
+import { GroupCard } from 'src/entities/group-card.entity';
 @Module({
+  imports: [TypeOrmModule.forFeature([Card,GroupCard])],
   providers: [CardService],
   controllers: [CardController]
 })
