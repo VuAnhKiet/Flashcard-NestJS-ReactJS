e24183627b99a92f2d06d1ca252e65fef65c3c96
Kiet <honeywork175@gmail.com>
2025-03-19 17:27:07 +0700

    Sharecard's module

--- src/modules/share-card-section/share-card-section.module.ts
+++ src/modules/share-card-section/share-card-section.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ShareCardSectionService } from './share-card-section.service';
 import { ShareCardSectionController } from './share-card-section.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ShareSection } from 'src/entities/share-section.entity';
+import { User } from 'src/entities/user.entity';
+import { Card } from 'src/entities/card.entity';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([ShareSection,User,Card])],
   providers: [ShareCardSectionService],
   controllers: [ShareCardSectionController]
 })
