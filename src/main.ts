650814a3954af2f4960e0c40e0142bf7c60c785b
Kiet <honeywork175@gmail.com>
2025-03-19 17:14:24 +0700

    Add cookies parser + cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
+import * as cookieParser from 'cookie-parser';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+  app.use(cookieParser());
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    
+  });
+  
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
