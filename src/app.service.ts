007f2ddf997454624640ecfbc4dc750cd0a77285
Kiet <honeywork175@gmail.com>
2025-03-19 17:13:07 +0700

    App's services

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
-import { Repository } from 'typeorm';
+
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World 5000 times!';
-  }
 }
