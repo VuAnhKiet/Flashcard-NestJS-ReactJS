e102c02189646777997784b194d33fce3da3f4a9
Kiet <honeywork175@gmail.com>
2025-03-19 17:10:53 +0700

    User's modules

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,10 +3,13 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
+import { FriendRequest } from 'src/entities/friend-request.entity';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User, FriendRequest])
+  ],
   providers: [UserService],
   controllers: [UserController]
 })
-export class UserModule {}
+export class UserModule { }
