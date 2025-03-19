01f61eb1a73618eb664a0b334453f6bbacda43ac
Kiet <honeywork175@gmail.com>
2025-03-19 17:27:31 +0700

    Groupcard's controller

--- src/modules/group-card/group-card.controller.ts
+++ src/modules/group-card/group-card.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query, Req, Res, UseGuards } from '@nestjs/common';
+import { GroupCardService } from './group-card.service';
+import { AuthGuard } from '../auth/guard/jwt.guard';
+import { CreateGroupCardDto } from 'src/dto/group-card/create-groupcard.dto';
+import { UpdateGroupCardDto } from 'src/dto/group-card/update-groupcard.dto';
 
 @Controller('group-card')
-export class GroupCardController {}
+@UseGuards(AuthGuard)
+export class GroupCardController {
+    constructor(private groupCardService: GroupCardService) { }
+
+    @Get()
+    async getGroupCard(@Req() req: Request | any): Promise<any> {
+        const userId = req.tokenPayload.id;
+        return this.groupCardService.getGroupCards(userId);
+    }
+
+    @Get('byId/:id')
+    async getCardsByGroupId(@Param('id') id: number) {
+        return this.groupCardService.getCardsByGroupId(id);
+    }
+
+    @Post()
+    async createGroupCard(@Body() createGroupCardDto: CreateGroupCardDto, @Req() req: Request | any) {
+        const userId = req.tokenPayload.id;
+        return this.groupCardService.createGroupCard(createGroupCardDto, userId);
+    }
+
+    @Delete(':id')
+    async deleteGroupCard(@Param('id') id: number, @Req() req: Request | any) {
+        const userId = req.tokenPayload.id;
+        return this.groupCardService.deleteGroupCard(id, userId);
+    }
+
+    @Put(':id')
+    async updateGroupCard(
+        @Param('id') id: number, 
+        @Body() updateGroupCardDto: UpdateGroupCardDto, 
+        @Req() req:Request|any
+    ) {
+        const userId = req.tokenPayload.id;
+        return this.groupCardService.updateGroupCard(id, updateGroupCardDto, userId);
+    }
+
+    @Get('search')
+    async searchGroupCard(@Query('s') searchTerm: string, @Req() req:Request|any) {
+        const userId = req.tokenPayload.id;
+        return this.groupCardService.searchGroupCard(searchTerm, userId);
+    }
+}
